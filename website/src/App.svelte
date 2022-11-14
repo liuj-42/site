8cccfaf033bfe41db1ab1a5cc6d4632dc4513f67
James Liu <liuxjames88@gmail.com>
2022-11-13 21:24:38 -0500

    change site to svelte

---
+++ website/src/App.svelte
@@ -0,0 +1,85 @@
+<script>
+</script>
+
+<main>
+
+  <h1>Hi there!</h1>
+  <div>
+    <p>My name is James Liu, and I am a Junior attending Renssaelear Polytechnic Institute (RPI). I am dual majoring in Computer Science and Information Technology & Web Science. </p>
+    <p>I love learning new things in my free time - here a some things that I’ve learned in school and some things that I have learned myself:</p>
+    <table>
+      <thead>
+        <td>School</td>
+        <td>Outside of School</td>
+      </thead>
+      <tbody>
+        <tr>
+          <td>C++</td>
+          <td>Vue</td>
+        </tr>
+        <tr>
+          <td>C</td>
+          <td>React</td>
+        </tr>
+        <tr>
+          <td>Java</td>
+          <td>Svelte</td>
+        </tr>
+        <tr>
+          <td>JavaScript</td>
+          <td>Netlify</td>
+        </tr>
+        <tr>
+          <td>HTML/CSS</td>
+          <td>Heroku</td>
+        </tr>
+        <tr>
+          <td>jQuery</td>
+          <td>Linux</td>
+        </tr>
+        <tr>
+          <td>Angular</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>MongoDB</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Node.js + Express</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>PHP</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>SQL</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    <p>Here are a few projects that I've worked on / am currently working on:</p>
+    
+    
+  </div>
+
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
